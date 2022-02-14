3ea4179de6a943f784a309a6e4f1263fe96dfa56
TheW00Xer <thew00xer@gmail.com>
2022-02-14 17:09:59 +0900

    Update of handling empty text box or value = "0" zero for dividing function.  If the number text box is empty app converts it to 0 automatically. Dividing by zero "0" shows "Error" as result.

--- app/src/main/java/com/example/mycalc/MainActivity.java
+++ app/src/main/java/com/example/mycalc/MainActivity.java
@@ -2,100 +2,82 @@ package com.example.mycalc;
 
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    public int getInt(EditText editText) {              //getInt is function name, (EditText editText) is declaration of object type we are working with and it's name
+        String text = (editText.getText().toString());  //we created variable "text" it's type is String and it is equal to value of editText which we got (.getText) and converted it to String (.toString)
+        if (text.equals("")) {                          //possible to use "==" instead of equal
+            editText.setText("0");                      //after executing function we set "editText" value to 0 if the condition is met
+            return 0;                                   //return ends this section "if" of the function
+        }
+        else {
+            return Integer.parseInt(text);              //if condition isn't met function will return us the original value of variable "text"
+        }
+    }
+    //EXAMPLE FUNCTION
+    //public int function(int value) {
+    //    if (value >1) {
+    //        return 1;
+    //    }
+    //    else {
+    //        return 0;
+    //    }
+    //}
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        EditText numberOne = (EditText) findViewById(R.id.editTextNumber1);
-        EditText numberTwo = (EditText) findViewById(R.id.editTextNumber2);
-        TextView textViewResult = (TextView) findViewById(R.id.textViewResult);
-        Button buttonPlus = (Button) findViewById(R.id.buttonPlus);
-        buttonPlus.setOnClickListener(new View.OnClickListener() {
+        EditText numberOne = findViewById(R.id.editTextNumber1);
+        EditText numberTwo = findViewById(R.id.editTextNumber2);
+        TextView textViewResult = findViewById(R.id.textViewResult);
 
-            public void onClick(View view) {
-                if (numberOne.getText().toString().equals("")){
-                    numberOne.setText(String.valueOf(0));
-                }
-                int one = Integer.parseInt(String.valueOf(numberOne.getText()));
-                if (numberTwo.getText().toString().equals("")){
-                    numberTwo.setText(String.valueOf(0));
-                }
-                int two = Integer.parseInt(String.valueOf(numberTwo.getText()));
+        Button buttonPlus = findViewById(R.id.buttonPlus);
+        buttonPlus.setOnClickListener(view -> {
+            int one = getInt(numberOne);
+            int two = getInt(numberTwo);
 
-                int resultPlus = one + two;
-                Log.i("MainActivity", String.valueOf(resultPlus));
-                textViewResult.setText(String.valueOf(resultPlus));
-
-
-            }
+            int resultOf = one + two;
+            Log.i("MainActivity", String.valueOf(resultOf));
+            textViewResult.setText(String.valueOf(resultOf));
         });
+
         Button buttonMinus = (Button) findViewById(R.id.buttonMinus);
-        buttonMinus.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                if (numberOne.getText().toString().equals("")){
-                    numberOne.setText(String.valueOf(0));
-                }
-                int one = Integer.parseInt(String.valueOf(numberOne.getText()));
-                if (numberTwo.getText().toString().equals("")){
-                    numberTwo.setText(String.valueOf(0));
-                }
-                int two = Integer.parseInt(String.valueOf(numberTwo.getText()));
+        buttonMinus.setOnClickListener(view -> {
+            int one = getInt(numberOne);
+            int two = getInt(numberTwo);
 
-                int resultPlus = one - two;
-                Log.i("MainActivity", String.valueOf(resultPlus));
-                textViewResult.setText(String.valueOf(resultPlus));
-            }
+            int resultOf = one - two;
+            Log.i("MainActivity", String.valueOf(resultOf));
+            textViewResult.setText(String.valueOf(resultOf));
         });
+
         Button buttonMultiply = (Button) findViewById(R.id.buttonMultiply);
-        buttonMultiply.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                if (numberOne.getText().toString().equals("")){
-                    numberOne.setText(String.valueOf(0));
-                }
-                int one = Integer.parseInt(String.valueOf(numberOne.getText()));
+        buttonMultiply.setOnClickListener(view -> {
+            int one = getInt(numberOne);
+            int two = getInt(numberTwo);
 
-                if (numberTwo.getText().toString().equals("")){
-                    numberTwo.setText(String.valueOf(0));
-                }
-                int two = Integer.parseInt(String.valueOf(numberTwo.getText()));
-
-                int resultPlus = one * two;
-                Log.i("MainActivity", String.valueOf(resultPlus));
-                textViewResult.setText(String.valueOf(resultPlus));
-            }
+            int resultOf = one * two;
+            Log.i("MainActivity", String.valueOf(resultOf));
+            textViewResult.setText(String.valueOf(resultOf));
         });
+
         Button buttonDivide = (Button) findViewById(R.id.buttonDivide);
-        buttonDivide.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                if (numberOne.getText().toString().equals("")){
-                    numberOne.setText(String.valueOf(1));
-                }
-                if (numberOne.getText().toString().equals("0")){
-                    numberOne.setText(String.valueOf(1));
-                }
-                int one = Integer.parseInt(String.valueOf(numberOne.getText()));
-
-                if (numberTwo.getText().toString().equals("")){
-                    numberTwo.setText(String.valueOf(1));
-                }
-                if (numberTwo.getText().toString().equals("0")){
-                    numberTwo.setText(String.valueOf(1));
-                }
-                int two = Integer.parseInt(String.valueOf(numberTwo.getText()));
-
-                int resultPlus = one / two;
-                Log.i("MainActivity", String.valueOf(resultPlus));
-                textViewResult.setText(String.valueOf(resultPlus));
+        buttonDivide.setOnClickListener(view -> {
+            int one = getInt(numberOne);
+            int two = getInt(numberTwo);
+            if (two == 0) {
+                textViewResult.setText(R.string.errorMessage);
+            }
+            else {
+                int resultOf = one / two;
+                Log.i("MainActivity", String.valueOf(resultOf));
+                textViewResult.setText(String.valueOf(resultOf));
             }
         });
     }
